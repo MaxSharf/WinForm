bbf1139467468cde25cbc8626f41ecacc3a4a56b
Sara_xy66@student.itstep.org <Sara_xy66@student.itstep.org>
2022-10-03 19:49:22 +0300

    +

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace Lb_1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            
+            Console.WriteLine("rfrfrfrf");
         }
     }
 }
